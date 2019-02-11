27a3615d09e8670c65f339f0ad5d4f68af0d2816
Nick Inman <nick@theoatmeal.com>
2019-02-11 21:52:28 +0000

    Automatically backed up by Learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -3,12 +3,12 @@ class Author < ActiveRecord::Base
   validates :email, uniqueness: true
 
   def create
-    @author = Author.new(params)
+    @author = Author.new(author_params)
     if @author.valid?
       @author.save
       redirect_to author_path(@author)
     else
-      render :'authors/new'
+      render :new
     end
   end
 end
