523f25dd30d83f7a6a24622040699ad02a93018b
Nick Inman <nick@theoatmeal.com>
2019-02-11 21:58:33 +0000

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,14 @@
 class Author < ActiveRecord::Base
+  validates :name, presence: true
+  validates :email, uniqueness: true
+
+  def create
+    @author = Author.new(params)
+    if @author.valid?
+      @author.save
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
+  end
 end
